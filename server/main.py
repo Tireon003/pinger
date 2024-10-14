31cff09cc24f3c50d2e3a34f54ece866d5277c84
Tireon003 <gunash29@mail.ru>
2024-10-14 23:29:11 +0300

    created fastapi entry point

---
+++ server/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+
+app = FastAPI(
+    redoc_url=None,
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+if __name__ == '__main__':
+    uvicorn.run(
+        app=app,
+        host='0.0.0.0',
+        port=8228,
+    )
